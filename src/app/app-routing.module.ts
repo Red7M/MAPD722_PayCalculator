08057afc81ca17d212536269c483df08c10ee404
Red7M <reda.mehali@gmail.com>
2021-03-03 13:12:24 -0500

    Create payCalculator app using Ionic, and Angular. Assignment 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'about',
+    loadChildren: () => import('./home/about.module').then( m => m.AboutPageModule)
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AboutPage } from './about.page';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  numberOfHours = 0;
+  hoursRate = 0;
+  regularPay = 0;
+  overtimePay = 0;
+  totalPay = 0;
+  tax = 0;
+  aboutPage: AboutPage;
 
-  constructor() {}
+  constructor(public navCtrl: NavController) {  }
 
+  calculateRegularPay() {
+    if (this.numberOfHours <= 40) {
+      console.log('Regular pay');
+      this.regularPay = this.numberOfHours * this.hoursRate;
+      this.overtimePay = 0;
+    } else {
+      console.log('Overtime pay');
+      this.overtimePay = (this.numberOfHours - 40) * this.hoursRate * 1.5;
+      this.regularPay = 40 * this.hoursRate;
+    }
+
+    // Total pay pre tax
+    this.totalPay = this.regularPay + this.overtimePay;
+
+    // Tax based on total pay
+    this.tax = this.totalPay * 0.18;
+  }
+
+  openAboutPage() {
+  }
 }
